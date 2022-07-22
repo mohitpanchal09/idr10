728bb0ca1aa871eebed006289be0f95c577400c4
unknown <devesh10tripathi@gmail.com>
2022-07-22 12:29:41 +0530

    initial commit

--- api/models/Product.js
+++ api/models/Product.js
@@ -7,11 +7,16 @@ const ProductSchema = new mongoose.Schema(
         img1 : {type:String , required:true },
         img2: {type:String , required:true },
         img3: {type:String , required:true },
-        features : {type:Array },
-        features2 : {type:Array },
-        features3 : {type:Array },
-        
-       
+        features : {type:String },
+        features1 : {type:String},
+        features2 : {type:String },
+        features3 : {type:String },
+        features4 : {type:String},
+        features5 : {type:String},
+        features6 : {type:String},
+        features7 : {type:String},
+        features8 : {type:String},
+        features9 : {type:String}
 
     },{timestamps:false}
 )
